f82a7357f66284cc1ecb945cbc76a148610920e3
morskiguru <ivanfanjek10@gmail.com>
2021-08-02 15:42:14 +0200

    "Finished location choice"

--- location_choice.py
+++ location_choice.py
@@ -1,4 +1,38 @@
+user_location_input = []
 
 
-def location_choice():
-    return True
+def location_choice(expanded_search=False):
+    while True:
+        print("Lets get started.")
+        if not expanded_search:
+            user_input = input("""
+            Please choose your location where you want to eat:
+            1) Center of Town
+            2) West Side of Town
+            3) East Side of Town
+            4) North Side of Town
+            5) South Side of Town
+            6) Outskirts of town
+            """)
+            if int(user_input) in list(range(1, 7, 1)):
+                user_location_input.append(int(user_input))
+                break
+        if expanded_search:
+            print("""
+            Add in which locations you may want to have a lovely meal
+            1) Center of Town
+            2) West Side of Town
+            3) East Side of Town
+            4) North Side of Town
+            5) South Side of Town
+            6) Outskirts of town
+            7) Added all needed locations
+            """)
+            while True:
+                user_expanded_location = input("")
+                if int(user_expanded_location) == 7:
+                    print("Finished adding locations")
+                    break
+                elif int(user_expanded_location) in list(range(1, 7, 1)):
+                    user_location_input.append(int(user_expanded_location))
+            break
--- main_recommendation.py
+++ main_recommendation.py
@@ -1,13 +1,18 @@
 #import files
 from intro import intro
+from location_choice import location_choice, user_location_input
 
 temporary = open("Dict_data.py", "r")
 dictionary_data = temporary.read()
+location=[]
 reset = True
 
 while reset:
     intro()
-    print("aaaaaaaaaa")
+    location_choice()
+
+
+
 
 
 
